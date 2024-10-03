14111aa3ccc9d987bd429e0a7738251106bc30cd
sebvstiansoto <sebastian.soto.c06@gmail.com>
2024-10-03 19:18:02 -0300

    #1

--- src/app.py
+++ src/app.py
@@ -1,33 +1,64 @@
-from flask import Flask, jsonify, request
+import os
+from flask import Flask, request, jsonify, url_for
+from flask_cors import CORS
+from utils import APIException, generate_sitemap
+from datastructures import FamilyStructure
+
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+CORS(app)
 
-todos = [
-    { "label": "My first task", "done": False },
-    { "label": "My second task", "done": False }
-]
+jackson_family = FamilyStructure("Jackson")
 
-@app.route('/todos', methods=['GET'])
-def hello_world():
-    return jsonify(todos)
+@app.errorhandler(APIException)
+def handle_invalid_usage(error):
+    return jsonify(error.to_dict()), error.status_code
 
-@app.route('/todos', methods=['POST'])
-def add_new_todo():
-    request_body = request.json
-    print("Incoming request with the following body", request_body)
-    todos.append(request_body)
-    return 'Response for the POST todo'
+@app.route('/')
+def sitemap():
+    return generate_sitemap(app)
 
-@app.route('/todos/<int:position>', methods=['DELETE'])
-def delete_todo(position):
-    print("This is the position to delete:", position)
+@app.route('/members', methods=['GET'])
+def get_all_members():
+    members = jackson_family.get_all_members()
+    return jsonify(members), 200
 
-    if 0 <= position < len(todos):
-        todos.pop(position)
+@app.route('/add_member', methods=['POST'])
+def add_member():
+    new_member = request.json
+    member = jackson_family.add_member(new_member)
+    if member is not None:
+        members = jackson_family.get_all_members()
+        response = {
+            "new_added": member,
+            "family": members
+        }
+        return jsonify(response), 200
     else:
-        return jsonify({"error": "Invalid position"}),
-    
-    return jsonify(todos),
+        response = {
+            "response": f"El miembro con el nombre {new_member['first_name']} ya existe",
+        }
+        return jsonify(response), 400
 
+@app.route('/delete_member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.delete_member(member_id)
+    if member is None:
+        response = {
+            "response": f"El miembro con el id {member_id} no existe",
+        }
+        return jsonify(response), 404
+    else:
+        response = {
+            "done": True
+        }
+        return jsonify(response), 200   
+    
+    
+
+
+# this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=True)
---
+++ src/datastructures.py
@@ -0,0 +1,49 @@
+class FamilyStructure:
+    def __init__(self, last_name):
+        self.last_name = last_name
+        self._next_id = 1
+        self._members = [
+            {
+                "id": self._generate_id(),
+                "first_name": "John",
+                "last_name": last_name,
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
+
+    def _generate_id(self):
+        generated_id = self._next_id
+        self._next_id += 1
+        return generated_id
+
+    def add_member(self, member):
+        member["id"] = self._generate_id()
+        member["last_name"] = self.last_name
+        self._members.append(member)
+
+    def delete_member(self, id):
+        self._members = [member for member in self._members if member["id"] != id]
+
+    def get_member(self, id):
+        for member in self._members:
+            if member["id"] == id:
+                return member
+        return None
+
+    def get_all_members(self):
+        return self._members
